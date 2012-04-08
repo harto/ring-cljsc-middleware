b90ab5bc2c78e04f81bd050e3a3044a5f9273609
Stuart Campbell <stuart@harto.org>
2012-04-08 19:01:59 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Ring middleware that automatically (re)compiles your ClojureScript
+files.
+
+There are two functions in the `ring.middleware.cljsc` namespace:
+
+  * `wrap-recompile` is a wrapper around `cljs.closure/build` that
+    recompiles files as required.
+
+  * `wrap-encoding` adds a UTF-8 encoding header to all JavaScript
+    file responses.
+
+Usage:
+
+    (ns foo
+      (:require [ring.middleware.cljsc :as cljsc])
+    
+    (def app (-> my-route-handler
+                 (cljsc/wrap-recompile "src")
+                 (cljsc/wrap-encoding)))
---
+++ src/ring/middleware/cljsc.clj
@@ -0,0 +1,35 @@
+(ns ring.middleware.cljsc
+  "Ring middleware that automatically (re)compiles your ClojureScript files."
+  (:require [clojure.tools.logging :as log]
+            [cljs.closure :as cljsc]))
+
+(defonce ^:private last-compile
+  (atom nil))
+
+(defn wrap-recompile
+  "Recompiles ClojureScript files in `src-dir` if one of `files` is modified
+   since last compile time.
+
+   An optional map of options may be provided, which is passed directly to
+   `cljs.closure/build`."
+  ([handler files src-dir]
+     (wrap-recompile files src-dir {}))
+  ([handler files src-dir opts]
+     (fn [request]
+       (let [newest (apply max 0 (map #(.lastModified %) files))]
+         (when (> newest (or @last-compile 0))
+           (log/info "recompiling")
+           (reset! last-compile ts)
+           (cljsc/build src-dir opts)))
+       (handler request))))
+
+(defn wrap-encoding
+  "Provides the correct character encoding header for compiled JavaScript
+   files."
+  [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (if (.endsWith (:uri request) ".js")
+        (header response "Content-Type" "text/javascript; charset=utf-8")
+        response))))
+
