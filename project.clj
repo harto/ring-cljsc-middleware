d56e252fe74577a34586ea6ab5e24cacac5b3aee
Stuart Campbell <stuart@harto.org>
2012-06-29 21:00:57 +1000

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ring-cljsc-middleware "0.0.2"
   :description "Ring middleware that automatically (re)compiles your ClojureScript files."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/clojurescript "0.0-1011"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojurescript "0.0-1236"]
                  [org.clojure/tools.logging "0.2.3"]])
