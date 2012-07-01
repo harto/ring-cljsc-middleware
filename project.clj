18ca86faa65bfa60be832c3ca5e4a09cb451972d
Stuart Campbell <stuart@harto.org>
2012-07-01 22:12:51 +1000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-cljsc-middleware "0.0.2"
+(defproject ring-cljsc-middleware "0.0.3"
   :description "Ring middleware that automatically (re)compiles your ClojureScript files."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojurescript "0.0-1424"]
